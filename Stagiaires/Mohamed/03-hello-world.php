ea3b7e2113842f9e912547a5ce67acfd5e0847f8
eiji-hbrs <eiji.haubursin@outlook.com>
2026-03-04 15:06:34 +0100

    exo 3

---
+++ Stagiaires/Mohamed/03-hello-world.php
@@ -0,0 +1,16 @@
+<?php
+// echo sans concaténation
+echo "Hello World":
+echo "<br>":
+// possible de concaténer avec la -, avec echo (consiel: utiliser le point tout le temps pour éviter les errerurs)
+echo "Hello freinds , !";
+
+// 
+?>
+<hr>
+<?php
+// print avec concaténation
+print "Hello World";
+print "<br>";
+// seulement possible de concaténer avec le -. avec print
+print "Hello friends . !";
